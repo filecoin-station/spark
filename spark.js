6e91bb455e9be469b966fb26d789a32c0ff980d0
Julian Gruber <julian@juliangruber.com>
2023-05-24 12:12:05 +0200

    add fetch car via saturn gateway

--- spark.js
+++ spark.js
@@ -1,5 +1,21 @@
-const res = await fetch('https://spark.fly.dev/retrievals', {
-  method: 'POST'
-})
-const retrieval = await res.json()
+import { assert } from 'zinnia:assert'
+
+const getRetrieval = async () => {
+  const res = await fetch('https://spark.fly.dev/retrievals', {
+    method: 'POST'
+  })
+  return res.json()
+}
+
+const fetchCAR = async (cid) => {
+  assert(cid)
+  const url = `https://strn.pl/ipfs/${cid}`
+  console.log('fetching', url)
+  const res = await fetch(url)
+  return res.arrayBuffer()
+}
+
+const retrieval = await getRetrieval()
 console.log({ retrieval })
+const car = await fetchCAR(retrieval.cid)
+console.log('got CAR')
