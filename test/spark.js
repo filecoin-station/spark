6dc0cc92176e0bd73e1f2a5fa8b9febc4f89fabc
Julian Gruber <julian@juliangruber.com>
2023-05-29 02:41:18 +0200

    simplify, add unit test

--- test/spark.js
+++ test/spark.js
@@ -16,3 +16,15 @@ test('getRetrieval', async () => {
   const spark = new Spark({ fetch })
   assertEquals(await spark.getRetrieval(), retrieval)
 })
+
+test('submitRetrieval', async () => {
+  const fetch = async (url, opts) => {
+    assertEquals(url, 'https://spark.fly.dev/retrievals/0')
+    assertEquals(opts.method, 'PATCH')
+    assertEquals(JSON.parse(opts.body), { success: true })
+    assertEquals(opts.headers['Content-Type'], 'application/json')
+    return { status: 200 }
+  }
+  const spark = new Spark({ fetch })
+  await spark.submitRetrieval({ id: 0, success: true })
+})
