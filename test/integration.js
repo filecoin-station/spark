dfc6707d0dd12157b50526d47688355d04784b86
Julian Gruber <julian@juliangruber.com>
2023-05-31 15:26:58 +0200

    fix submitRetrieval + test

--- test/integration.js
+++ test/integration.js
@@ -3,13 +3,5 @@ import { test } from 'zinnia:test'
 
 test('integration', async () => {
   const spark = new Spark()
-  for (let i = 0; i < 100; i++) {
-    try {
-      await spark.nextRetrieval()
-      return
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  throw new Error('No retrieval succeeded')
+  await spark.nextRetrieval()
 })
