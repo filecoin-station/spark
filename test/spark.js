633798c624b0e7e733210cfbc487fc4744e6a518
Julian Gruber <julian@juliangruber.com>
2023-05-31 15:09:50 +0200

    add test order, arguments and length of `fetch` calls

--- test/spark.js
+++ test/spark.js
@@ -4,9 +4,9 @@ import { assertInstanceOf, assertEquals } from 'zinnia:assert'
 
 test('getRetrieval', async () => {
   const retrieval = { retrieval: 'retrieval' }
+  const requests = []
   const fetch = async (url, opts) => {
-    assertEquals(url, 'https://spark.fly.dev/retrievals')
-    assertEquals(opts.method, 'POST')
+    requests.push({ url, opts })
     return {
       async json () {
         return retrieval
@@ -15,13 +15,18 @@ test('getRetrieval', async () => {
   }
   const spark = new Spark({ fetch })
   assertEquals(await spark.getRetrieval(), retrieval)
+  assertEquals(requests, [{
+    url: 'https://spark.fly.dev/retrievals',
+    opts: { method: 'POST' }
+  }])
 })
 
 // TODO: test more cases
 test('fetchCAR', async () => {
   const URL = 'url'
+  const requests = []
   const fetch = async url => {
-    assertEquals(url, URL)
+    requests.push({ url })
     return {
       status: 200,
       ok: true,
@@ -37,10 +42,13 @@ test('fetchCAR', async () => {
   assertInstanceOf(stats.end, Date)
   assertEquals(stats.byteLength, 3)
   assertEquals(stats.status, 200)
+  assertEquals(requests, [{ url: URL }])
 })
 
 test('submitRetrieval', async () => {
+  const requests = []
   const fetch = async (url, opts) => {
+    requests.push({ url, opts })
     assertEquals(url, 'https://spark.fly.dev/retrievals/0')
     assertEquals(opts.method, 'PATCH')
     assertEquals(JSON.parse(opts.body), { success: true })
@@ -49,4 +57,14 @@ test('submitRetrieval', async () => {
   }
   const spark = new Spark({ fetch })
   await spark.submitRetrieval(0, { success: true })
+  assertEquals(requests, [
+    {
+      url: 'https://spark.fly.dev/retrievals/0',
+      opts: {
+        method: 'PATCH',
+        body: JSON.stringify({ success: true }),
+        headers: { 'Content-Type': 'application/json' }
+      }
+    }
+  ])
 })
