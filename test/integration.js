33775e5dac1fbf60e7d060fcdb33ac146e3963fa
Julian Gruber <julian@juliangruber.com>
2023-05-31 17:42:28 +0200

    add assert res.ok

--- test/integration.js
+++ test/integration.js
@@ -6,6 +6,7 @@ test('integration', async () => {
   const spark = new Spark()
   const id = await spark.nextRetrieval()
   const res = await fetch(`https://spark.fly.dev/retrievals/${id}`)
+  assert(res.ok)
   const retrieval = await res.json()
   assert(retrieval.startAt)
   assert(retrieval.finishedAt)
