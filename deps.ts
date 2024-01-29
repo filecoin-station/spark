ff7a138c0cd426e5973c3c5e22d3ffd93d5b974d
Miroslav Bajtoš <oss@bajtos.net>
2024-01-29 16:39:25 +0100

    deps: add deno-varint
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

---
+++ deps.ts
@@ -0,0 +1,7 @@
+// 3rd-party dependencies from Denoland
+//
+// Run the following script after making change in this file:
+//   deno bundle deps.ts vendored/deps.js
+//
+
+export { decode as decodeVarint } from 'https://deno.land/x/varint@v2.0.0/varint.ts'
