038d4b7a27b9ad679cf97ddd5fd9a18549912551
Miroslav Bajtoš <oss@bajtos.net>
2024-02-01 11:17:02 +0100

    feat: resolve the retrieval provider using IPNI (#51)
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

---
+++ deps.ts
@@ -0,0 +1,9 @@
+// 3rd-party dependencies from Denoland
+//
+// Run the following script after making change in this file:
+//   deno bundle deps.ts vendor/deno-deps.js
+//
+
+export { encodeHex } from 'https://deno.land/std@0.203.0/encoding/hex.ts'
+export { decodeBase64 } from 'https://deno.land/std@0.203.0/encoding/base64.ts'
+export { decode as decodeVarint } from 'https://deno.land/x/varint@v2.0.0/varint.ts'
--- test.js
+++ test.js
@@ -1,2 +1,3 @@
+import './test/ipni-client.test.js'
 import './test/integration.js'
 import './test/spark.js'
