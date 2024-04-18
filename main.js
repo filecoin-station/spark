48e031038fa690339b17382570fcbfb0e3ab6e92
Miroslav Bajtoš <oss@bajtos.net>
2024-04-18 11:14:40 +0200

    feat: log error activity when no `stationId`
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import Spark from './lib/spark.js'
 
+if (!Zinnia.stationId) {
+  Zinnia.activity.error('Your Station version is outdated. Please upgrade it as soon as possible.')
+}
+
 const spark = new Spark()
 await spark.run()
