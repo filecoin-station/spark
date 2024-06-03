2e472362af40381e1ccfe81d5ff0dcfec4a2756c
Miroslav Bajtoš <oss@bajtos.net>
2024-06-03 12:58:59 +0200

    fix: improve logs (#73)
    Example output before my change:
    
    ```
    Retrieval failed with status code 502: no candidates found
    
    {
      timeout: false,
      startAt: 2024-05-31T07:30:43.389Z,
      // etc.
      protocol: "graphsync",
      providerAddress: "/ip4/118.212.68.22/tcp/24501/p2p/12D3KooWJJEL214g7yMFMFaE6aPRaQTLVVCNFjWeiBLUCTzP98tp"
    }
    Submitting measurement...
    {
      sparkVersion: "1.11.4",
      zinniaVersion: "0.19.1",
      cid: "bafykbzacechhbxnsn5dw6lx7phcfvkjc6kqsuuwl4bnynexewb5y3vtcw2fvw",
      minerId: "f02894875",
      timeout: false,
      startAt: 2024-05-31T07:30:43.389Z,
      // etc.
      providerAddress: "/ip4/118.212.68.22/tcp/24501/p2p/12D3KooWJJEL214g7yMFMFaE6aPRaQTLVVCNFjWeiBLUCTzP98tp",
      participantAddress: "0x000000000000000000000000000000000000dEaD",
      stationId: "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
    }
    ```
    
    This commit removes the first log which was printed when `fetch()`
    was able to get some HTTP response frome the server.
    
    It is also removing the trailing newline from HTTP error response bodies
    before we print them to the console.
    
    Finally, it adds an empty line between tasks, to visually delimit
    logs belonging to different tasks.

--- manual-check.js
+++ manual-check.js
@@ -13,3 +13,4 @@ const minerId = 'f010479'
 const spark = new Spark()
 const stats = { cid, minerId, indexerResult: null, statusCode: null, byteLength: 0 }
 await spark.executeRetrievalCheck({ cid, minerId }, stats)
+console.log('Measurement: %o', stats)
