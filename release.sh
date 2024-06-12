8e4d27520dcd95054b77d0adb0f9d91651f76917
Miroslav Bajtoš <oss@bajtos.net>
2024-06-12 16:47:31 +0200

    release.sh: fail on dirty git
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+git diff --quiet HEAD || (echo "please revert the uncommited changes"; exit 1)
+
 SPARK_VERSION="${1?Missing required argument: semver}"
 
 sed -i '' -e "s/SPARK_VERSION = .*/SPARK_VERSION = '$SPARK_VERSION'/" lib/constants.js
-git add .
+git add lib/constants.js
 git commit -m v"$SPARK_VERSION"
 git tag -s v"$SPARK_VERSION" -m v"$SPARK_VERSION"
 git push
