4a5821759f5fd3b967ae3ff9dc22f8fdfedd1eec
Miroslav Bajtoš <oss@bajtos.net>
2024-07-10 09:22:10 +0100

    fix: reject non-semver strings in release.sh
    Fail when the next version is specified as a non-semver string,
    e.g. "minor" or "patch".
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- release.sh
+++ release.sh
@@ -5,6 +5,11 @@ git diff --quiet HEAD || (echo "please revert the uncommited changes"; exit 1)
 
 SPARK_VERSION="${1?Missing required argument: semver}"
 
+(echo "$SPARK_VERSION" | grep -Eq  '^\d+\.\d+\.\d+$') || {
+  echo Invalid version string \'$SPARK_VERSION\'. Must be MAJOR.MINOR.PATCH
+  exit 1
+}
+
 sed -i '' -e "s/SPARK_VERSION = .*/SPARK_VERSION = '$SPARK_VERSION'/" lib/constants.js
 git add lib/constants.js
 git commit -m v"$SPARK_VERSION"
