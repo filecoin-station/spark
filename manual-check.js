ce16187466e07a4cf11de7ac6d34763cdb900488
Miroslav Bajtoš <oss@bajtos.net>
2024-08-09 14:23:23 +0200

    feat: more info when manual check fails (#89)
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- manual-check.js
+++ manual-check.js
@@ -3,7 +3,7 @@
 // zinnia run manual-check.js
 //
 
-import Spark from './lib/spark.js'
+import Spark, { getRetrievalUrl } from './lib/spark.js'
 
 // The task to check, replace with your own values
 const cid = 'bafybeiepi56qxfcwqgpstg25r6sonig7y3pzd37lwambzmlcmbnujjri4a'
@@ -14,3 +14,36 @@ const spark = new Spark()
 const stats = { cid, minerId, indexerResult: null, statusCode: null, byteLength: 0 }
 await spark.executeRetrievalCheck({ cid, minerId }, stats)
 console.log('Measurement: %o', stats)
+
+if (stats.providerAddress && stats.statusCode !== 200) {
+  console.log('\nThe retrieval failed.')
+  switch (stats.protocol) {
+    case 'graphsync':
+      console.log('You can get more details by running Lassie manually:\n')
+      console.log(
+        '  lassie fetch -o /dev/null -vv --dag-scope block --protocols graphsync --providers %s %s',
+        JSON.stringify(stats.providerAddress),
+        cid
+      )
+      console.log('\nHow to install Lassie: https://github.com/filecoin-project/lassie?tab=readme-ov-file#installation')
+      break
+    case 'http':
+      try {
+        const url = getRetrievalUrl(stats.protocol, stats.providerAddress, cid)
+        console.log('You can get more details by requesting the following URL yourself:\n')
+        console.log('  %s', url)
+        console.log('\nE.g. using `curl`:')
+        console.log('  curl -i %s', JSON.stringify(url))
+        console.log('\nYou can also test the retrieval using Lassie:\n')
+        console.log(
+          '  lassie fetch -o /dev/null -vv --dag-scope block --protocols http --providers %s %s',
+          JSON.stringify(stats.providerAddress),
+          cid
+        )
+        console.log('\nHow to install Lassie: https://github.com/filecoin-project/lassie?tab=readme-ov-file#installation')
+      } catch (err) {
+        console.log('The provider address %j cannot be converted to a URL: %s', stats.providerAddress, err.message ?? err)
+      }
+      break
+  }
+}
