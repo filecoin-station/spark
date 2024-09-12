3f8fc20ce6d7de7bc2b956b6d54f835e5cf69943
Miroslav Bajtoš <oss@bajtos.net>
2024-09-12 15:31:53 +0200

    refactor: move cid & minerId to a task object
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- manual-check.js
+++ manual-check.js
@@ -7,8 +7,10 @@ import Spark, { getRetrievalUrl } from './lib/spark.js'
 import { getMinerPeerId as defaultGetMinerPeerId } from './lib/miner-info.js'
 
 // The task to check, replace with your own values
-const cid = 'bafkreih25dih6ug3xtj73vswccw423b56ilrwmnos4cbwhrceudopdp5sq'
-const minerId = 'f0frisbii'
+const task = {
+  cid: 'bafkreih25dih6ug3xtj73vswccw423b56ilrwmnos4cbwhrceudopdp5sq',
+  minerId: 'f0frisbii'
+}
 
 const getMinerPeerId = (minerId) =>
   minerId === 'f0frisbii'
@@ -17,8 +19,8 @@ const getMinerPeerId = (minerId) =>
 
 // Run the check
 const spark = new Spark({ getMinerPeerId })
-const stats = { cid, minerId, indexerResult: null, statusCode: null, byteLength: 0 }
-await spark.executeRetrievalCheck({ cid, minerId }, stats)
+const stats = { ...task, indexerResult: null, statusCode: null, byteLength: 0 }
+await spark.executeRetrievalCheck(task, stats)
 console.log('Measurement: %o', stats)
 
 if (stats.providerAddress && stats.statusCode !== 200) {
@@ -29,13 +31,13 @@ if (stats.providerAddress && stats.statusCode !== 200) {
       console.log(
         '  lassie fetch -o /dev/null -vv --dag-scope block --protocols graphsync --providers %s %s',
         JSON.stringify(stats.providerAddress),
-        cid
+        task.cid
       )
       console.log('\nHow to install Lassie: https://github.com/filecoin-project/lassie?tab=readme-ov-file#installation')
       break
     case 'http':
       try {
-        const url = getRetrievalUrl(stats.protocol, stats.providerAddress, cid)
+        const url = getRetrievalUrl(stats.protocol, stats.providerAddress, task.cid)
         console.log('You can get more details by requesting the following URL yourself:\n')
         console.log('  %s', url)
         console.log('\nE.g. using `curl`:')
@@ -44,7 +46,7 @@ if (stats.providerAddress && stats.statusCode !== 200) {
         console.log(
           '  lassie fetch -o /dev/null -vv --dag-scope block --protocols http --providers %s %s',
           JSON.stringify(stats.providerAddress),
-          cid
+          task.cid
         )
         console.log('\nHow to install Lassie: https://github.com/filecoin-project/lassie?tab=readme-ov-file#installation')
       } catch (err) {
